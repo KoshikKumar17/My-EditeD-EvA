18445180c78bd2e0267d0769aa960788f528f1ec
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-05 17:25:19 +0530

    Create ping.py

---
+++ plugins/😁Fun by @KoshikKumar17/ping.py
@@ -0,0 +1,11 @@
+#by saminsumesh...
+import time
+from pyrogram import Client, filters
+from info import (
+    COMMAND_HAND_LER
+)
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+ALIVE = "Glad to hear that and really nice to know that I am not talking to a 👻."
+HELP = "CAACAgUAAxkBAAEDz6hh_mV3Lhc5piK2FdksyUx_r7QFYwACwwIAAscw4FfCkTbYQEv_rCME"
+
