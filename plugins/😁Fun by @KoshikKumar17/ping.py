9aa604920e10d60a3cc31a948c4de5831d42c00a
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-05 17:46:02 +0530

    ...

--- plugins/😁Fun by @KoshikKumar17/ping.py
+++ plugins/😁Fun by @KoshikKumar17/ping.py
@@ -6,6 +6,16 @@ from info import (
 )
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
-ALIVE = "Glad to hear that and really nice to know that I am not talking to a 👻."
 HELP = "CAACAgUAAxkBAAEDz6hh_mV3Lhc5piK2FdksyUx_r7QFYwACwwIAAscw4FfCkTbYQEv_rCME"
 
+@Client.on_message(filters.command("hi", COMMAND_HAND_LER) & f_onw_fliter)
+async def help_me(_, message):
+    await message.reply_sticker(HELP)
+
+@Client.on_message(filters.command("ping", COMMAND_HAND_LER) & f_onw_fliter)
+async def ping(_, message):
+    start_t = time.time()
+    rm = await message.reply_text("...🤔")
+    end_t = time.time()
+    time_taken_s = (end_t - start_t) * 1000
+    await rm.edit(f"Pong😜😜!\n{time_taken_s:.3f} ms\n \n **~ @KoshikKumar17**")
