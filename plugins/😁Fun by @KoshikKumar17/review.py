95ba068d0f9c543d174ab2e54880bb1811191fd2
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-09 06:59:06 +0530

    New

--- plugins/😁Fun by @KoshikKumar17/review.py
+++ plugins/😁Fun by @KoshikKumar17/review.py
@@ -3,12 +3,25 @@ import os
 from Script import script
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import ReplyKeyboardMarkup, one_time_keyboard, resize_keyboard
 
 @Client.on_message(filters.private & filters.command(["review"]))
 async def review(bot, update):
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton('✨ Click Here to Review ✨', url='https://comments.bot/thread/--pwqNLA2')]])
     await update.reply_text(
         text=script.REVIEW_TXT,
+        reply_markup = reply_markup,
+        disable_web_page_preview=True,
+    )
+
+@Client.on_message(filters.private & filters.command(["review"]))
+async def review(bot, update):
+    keyboard = [['.😘', '.😂', '.😃'],['.😔', '.👋', '.🆗']]
+    reply_markup = ReplyKeyboardMarkup(keyboard,
+                                       one_time_keyboard=True,
+                                       resize_keyboard=True)
+    await update.reply_text(
+        text=script.GETSTICKER_TXT.format(message.from_user.mention),
         reply_markup = reply_markup,
         disable_web_page_preview=True,
     )
