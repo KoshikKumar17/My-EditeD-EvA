b5c286dfb3b2bd713d2aedc07ff9370157492240
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-09 06:49:02 +0530

    👋👋

--- plugins/😁Fun by @KoshikKumar17/ping.py
+++ plugins/😁Fun by @KoshikKumar17/ping.py
@@ -15,6 +15,6 @@ async def ping(_, message):
     time_taken_s = (end_t - start_t) * 1000
     await rm.edit(f"Pong😜😜!\n{time_taken_s:.3f} ms\n \n **~ @KoshikKumar17**")
 
-@Client.on_message(filters.command("hi", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("👋", COMMAND_HAND_LER) & f_onw_fliter)
 async def help_me(_, message):
     await message.reply_sticker("CAACAgUAAxkBAAEDz8Zh_ni_q5DjjojrOO81rt6zUVXcgQAC6AMAAvKJ4VeIlT8Fu-vddyME")
