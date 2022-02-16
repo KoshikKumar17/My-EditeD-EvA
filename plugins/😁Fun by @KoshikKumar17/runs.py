46284e66a2cf58c55657382b1119b62f067a1e4d
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-02-16 18:47:17 +0530

    ..

--- plugins/😁Fun by @KoshikKumar17/runs.py
+++ plugins/😁Fun by @KoshikKumar17/runs.py
@@ -7,9 +7,9 @@ from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
 RUN_STRINGS = (
     "Aaj mere paas gaadi hai, bungla hai, paisa hai... tumhare paas kya hai?"
-    "Mere paas, mere paas... Maa hai...,'
-    "Rishte mein to hum tumhare baap lagte hain, naam hai Shahenshah'
-    "Kaun kambakth hai jo bardasht karne ke liye peeta hai. Main toh peeta hoon ke bas saans le sakoon
+    "Mere paas, mere paas... Maa hai...,"
+    "Rishte mein to hum tumhare baap lagte hain, naam hai Shahenshah"
+    "Kaun kambakth hai jo bardasht karne ke liye peeta hai. Main toh peeta hoon ke bas saans le sakoon"
     "Main aaj bhi pheke hue paise nahin uthata"
     "Pushpa, I hate tears... "
     "Bade bade shehron mein aisi chhoti chhoti baatein hoti rehti hain, Senorita."
@@ -21,14 +21,14 @@ RUN_STRINGS = (
    "ndaaz Apna Apna"
    "ndaaz Apna Apna"
    " jahan khade hote hai line wahin se chalu hoti hai kaliyan"
-    "Thapad se darr nahi lagta, pyaar se lagta hai”
+    "Thapad se darr nahi lagta, pyaar se lagta hai"
 )
 @Client.on_message(
     filters.command("runs", COMMAND_HAND_LER) &
     f_onw_fliter
 )
 async def runs(_, message):
-    """ .runs strings """
+    """ .runs"""
     effective_string = random.choice(RUN_STRINGS)
     if message.reply_to_message:
         await message.reply_to_message.reply_text(effective_string)
