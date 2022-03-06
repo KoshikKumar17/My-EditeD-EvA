a703f40743bcd488fe5e2bfc6648487ddb90d23e
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-06 09:14:29 +0530

    Create image2pdf.py

---
+++ plugins/😁Fun by @KoshikKumar17/image2pdf.py
@@ -0,0 +1,40 @@
+# (c) @KoshikKumar17
+import os
+from PIL import Image
+from pyrogram import Client,filters 
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
+
+@Client.on_message(filters.private & filters.photo)
+async def pdf(client,message):
+ 
+ if not isinstance(LIST.get(message.from_user.id), list):
+   LIST[message.from_user.id] = []
+
+  
+ 
+ file_id = str(message.photo.file_id)
+ ms = await message.reply_text("**Converting to PDF...😪**")
+ file = await client.download_media(file_id)
+ 
+ image = Image.open(file)
+ img = image.convert('RGB')
+ LIST[message.from_user.id].append(img)
+ await ms.edit(f"{len(LIST[message.from_user.id])} image  **Successfully created PDF** if you want add more image **Send me One by one**✌️\n\n **If done Click Here 👉 /convert** \n\n**@KoshikKumar17**")
+ 
+
+@Client.on_message(filters.command(['c2pdf']))
+async def done(client,message):
+ images = LIST.get(message.from_user.id)
+
+ if isinstance(images, list):
+  del LIST[message.from_user.id]
+ if not images:
+  await message.reply_text( "No image !!")
+  return
+
+ path = f"{message.from_user.id}" + "@KoshikKumar17.pdf"
+ images[0].save(path, save_all = True, append_images = images[1:])
+ 
+ await client.send_document(message.from_user.id, open(path, "rb"), caption = "**Heya!! Here your pdf !!**")
+ os.remove(path)
