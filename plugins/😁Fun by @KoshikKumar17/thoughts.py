850116a845e6f9d0237ec31bf187d3f214bfc548
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-06 22:16:28 +0530

    Create thoughts.py

---
+++ plugins/😁Fun by @KoshikKumar17/thoughts.py
@@ -0,0 +1,33 @@
+# (c) @KoshikKumar17
+import os
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+PX = "https://game-of-thrones-quotes.herokuapp.com/v1/"
+
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('🙋‍♂️ Made by 🙋‍♂️', url='https://t.me/KoshikKumar17')]])
+
+@Client.on_message(filters.command("joke"))
+async def get_thought(bot, update):
+    koshik = await update.reply_text("**Getting a Thought...😇**")
+    query = update.text.split(None, 1)[1]
+    reply_markup = BUTTONS
+    await koshik.edit_text(
+        text=gett_tht(query),
+        disable_web_page_preview=True,
+        reply_markup=reply_markup
+    )
+
+def gett_tht(type):
+    try:
+        r = requests.get(PX + requote_uri(type.lower()))
+        info = r.json()
+        thought = info['sentence']
+        writer = info['name']
+        gett_tht = f"""**{thought}**\n\n                           -->{writer}
+\n **@KoshikKumar17**"""
+        return gett_joke
+    except Exception as error:
+        return error
