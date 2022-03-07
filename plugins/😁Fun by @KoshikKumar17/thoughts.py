088e4aeee344fcdbd85d6f19f5b1624ea8e7bc2e
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-07 11:49:48 +0530

    Update thoughts.py

--- plugins/😁Fun by @KoshikKumar17/thoughts.py
+++ plugins/😁Fun by @KoshikKumar17/thoughts.py
@@ -5,16 +5,17 @@ from requests.utils import requote_uri
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-PX = "https://game-of-thrones-quotes.herokuapp.com/v1/random"
+PX = "https://game-of-thrones-quotes.herokuapp.com/v1/"
 
 BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('🙋‍♂️ Made by 🙋‍♂️', url='https://t.me/KoshikKumar17')]])
 
 @Client.on_message(filters.command("thought"))
 async def get_thought(bot, update):
     koshik = await update.reply_text("**I Am Processing...😇**")
+    query = update.text.split(None, 1)[1]
     reply_markup = BUTTONS
     await koshik.edit_text(
-        text=gett_tht,
+        text=gett_tht(query),
         disable_web_page_preview=True,
         reply_markup=reply_markup
     )
@@ -24,7 +25,8 @@ def gett_tht(type):
         r = requests.get(PX + requote_uri(type.lower()))
         info = r.json()
         thought = info['sentence']
-        gett_tht = f"""**{thought}**
+        qts = info['quotes']
+        gett_tht = f"""**{thought}** **{qts}**
 \n **@KoshikKumar17**"""
         return gett_tht
     except Exception as error:
