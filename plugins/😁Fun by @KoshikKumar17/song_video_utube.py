03a11a75bf5b668f6c4e5944770a610e540955e7
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-08 09:56:12 +0530

    Update song_video_utube.py

--- plugins/😁Fun by @KoshikKumar17/song_video_utube.py
+++ plugins/😁Fun by @KoshikKumar17/song_video_utube.py
@@ -257,7 +257,7 @@ async def vsong(client, message: Message):
     url = mo
     sedlyf = wget.download(kekme)
     opts = {
-        "format": "medium",
+        "format": "best",
         "addmetadata": True,
         "key": "FFmpegMetadata",
         "prefer_ffmpeg": True,
