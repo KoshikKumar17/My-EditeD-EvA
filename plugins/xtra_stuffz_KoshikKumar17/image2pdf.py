66a830de1cf636c00fb4f92b36a6922e5c1eb4af
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-09 17:43:26 +0530

    Update image2pdf.py

--- plugins/xtra_stuffz_KoshikKumar17/image2pdf.py
+++ plugins/xtra_stuffz_KoshikKumar17/image2pdf.py
@@ -16,13 +16,14 @@ async def pdf(client,message):
   
  
  file_id = str(message.photo.file_id)
- ms = await message.reply_text("**Converting to PDF...😪**")
+ ms = await message.reply_text("Converting to PDF ......")
  file = await client.download_media(file_id)
  
  image = Image.open(file)
  img = image.convert('RGB')
  LIST[message.from_user.id].append(img)
- await ms.edit(f"{len(LIST[message.from_user.id])} image  **Successfully created PDF** if you want add more image **Send me One by one**✌️\n\n **If done Click Here 👉 /c2pdf** \n\n**@KoshikKumar17**")
+ await ms.edit(f"{len(LIST[message.from_user.id])} image **Successfully created PDF**. If you want to add more image, Send me **One by One**\n\n If done **click** here 👉 __/c2pdf__")
+ 
 
 @Client.on_message(filters.command(['c2pdf']))
 async def done(client,message):
@@ -31,11 +32,11 @@ async def done(client,message):
  if isinstance(images, list):
   del LIST[message.from_user.id]
  if not images:
-  await message.reply_text( "No image !!")
+  await message.reply_text( "No image !!🤷🏻🤷🏻")
   return
 
  path = f"{message.from_user.id}" + "@KoshikKumar17.pdf"
  images[0].save(path, save_all = True, append_images = images[1:])
  
- await client.send_document(message.from_user.id, open(path, "rb"), caption = "**Heya!! Here your pdf !!**")
+ await client.send_document(message.from_user.id, open(path, "rb"), caption = "Heya!! Here your PDF !!")
  os.remove(path)
