f1377eb5dbdd5fb81e1bd8dd595316f93537a370
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-16 19:33:00 +0530

    Update jokes.py

--- plugins/xtra_stuffz_KoshikKumar17/jokes.py
+++ plugins/xtra_stuffz_KoshikKumar17/jokes.py
@@ -1,7 +1,7 @@
 # (c) @KoshikKumar17
 import os
 import requests
-from requests.utils import requote_uri
+import json
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
@@ -10,24 +10,18 @@ API = "https://v2.jokeapi.dev/joke/Any?type="
 BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('🙋‍♂️Father🙋‍♂️', url='https://t.me/KoshikKumar17')]])
 
 @Client.on_message(filters.command("joke"))
-async def reply_info(bot, update):
+async def jokeapibot(bot, update):
     koshik = await update.reply_text("Getting a Joke...😂")
-    query = update.text.split(None, 1)[1]
-    reply_markup = BUTTONS
-    await koshik.edit_text(
-        text=gett_joke(query),
-        disable_web_page_preview=True,
-        reply_markup=reply_markup
-    )
-
-def gett_joke(type):
-    try:
-        r = requests.get(API + requote_uri(type.lower()))
-        info = r.json()
-        joke = info['joke']
+    px = "https://v2.jokeapi.dev/joke/Any?type=single"
+        request = requests.get(px)
+        result = request.json()
+        joke = result['joke']
         gett_joke = f"""
 😁Here is Your Joke😁 :\n \n **{joke}**
 \nWith ❤️ by @KoshikKumar17"""
-        return gett_joke
-    except Exception as error:
-        return error
+    await koshik.edit_text(
+        text=gett_joke,
+        reply_markup=BUTTONS,
+        disable_web_page_preview=True
+   )
+    
