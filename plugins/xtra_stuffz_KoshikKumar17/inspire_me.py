9556c80023cc8c0e0746b98d219774fa063a98b8
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-18 11:28:01 +0530

    Update inspire_me.py

--- plugins/xtra_stuffz_KoshikKumar17/inspire_me.py
+++ plugins/xtra_stuffz_KoshikKumar17/inspire_me.py
@@ -1,7 +1,11 @@
 # © Siriustar
 
 import requests
+import pyrogram
 from pyrogram import Client, filters
+from pyrogram import *
+
+BTN = InlineKeyboardMarkup([[InlineKeyboardButton('💡 Source 💡', url='tg://openmessage?user_id=1857338892')],[InlineKeyboardButton('Inspire Me Again!!', callback_data='inspireagain')]])
 
 @Client.on_message(filters.command("inspire"))
 async def inspireme(bot, update):
@@ -11,3 +15,14 @@ async def inspireme(bot, update):
     img = get.text
     await update.reply_photo(photo=img, caption="Inspire me again! © Sirius")
     await s.delete()
+
+@Client.on_callback_query(filters.regex(r'^inspireagain'))
+async def inspiremecallbak(bot, update):
+    s = await update.reply_text("Processing...⏳",quote=True)
+    await update.answer('Generating.....⏳')
+    url = "http://inspirobot.me/api?generate=true"
+    get = requests.get(url)
+    img = get.text
+    await update.reply_photo(photo=img, caption="Inspire me again! © Sirius")
+    await s.delete()
+    
