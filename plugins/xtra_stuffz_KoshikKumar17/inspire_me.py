df3d5bd77a0024e1b51171bcee1fcd71a1ff7bdf
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-03-18 12:07:59 +0530

    Update inspire_me.py

--- plugins/xtra_stuffz_KoshikKumar17/inspire_me.py
+++ plugins/xtra_stuffz_KoshikKumar17/inspire_me.py
@@ -13,7 +13,7 @@ async def inspireme(bot, update):
     url = "http://inspirobot.me/api?generate=true"
     get = requests.get(url)
     img = get.text
-    await update.reply_photo(photo=img, caption="Inspire me again! © Sirius")
+    await update.reply_photo(photo=img, caption="Inspire me again! © Sirius", reply_markup=BTN)
     await s.delete()
 
 @Client.on_callback_query(filters.regex(r'^inspireagain'))
