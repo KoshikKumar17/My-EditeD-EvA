492cf0cabc658ddb4524f6407c18919e4ef1d217
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-01 10:15:48 +0530

    Update inspire_me.py

--- plugins/xtra_stuffz_KoshikKumar17/inspire_me.py
+++ plugins/xtra_stuffz_KoshikKumar17/inspire_me.py
@@ -6,8 +6,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 BTN = InlineKeyboardMarkup([[InlineKeyboardButton('💡 Source 💡', url='tg://openmessage?user_id=1857338892')],[InlineKeyboardButton('Inspire Me Again!!', callback_data='inspireagain')]])
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('💖🇮🇳✨ Made By ✨🇮🇳💖', url='https://t.me/KoshikKumar17')]])
-A = """{} with user id:- {} used /git command."""
 
 @Client.on_message(filters.command("inspire"))
 async def inspireme(bot, update):
@@ -17,40 +15,3 @@ async def inspireme(bot, update):
     img = get.text
     await update.reply_photo(photo=img, caption="Inspire me again! © Sirius", reply_markup=BTN, quote=True)
     await s.delete()
-
-@Koshik.on_message(filters.command("github", "git")
-async def getgithub(bot, message):
-    await update.reply_chat_action("typing")
-    un = message.text.split(None, 1)[1]
-    URL = f'https://api.github.com/users/{un}'
-    request = requests.get(URL)
-    result = request.json()
-    try:
-        url = result['html_url']
-        name = result['name']
-        company = result['company']
-        bio = result['bio']
-        created_at = result['created_at']
-        avatar_url = result['avatar_url']
-        blog = result['blog']
-        location = result['location']
-        repositories = result['public_repos']
-        followers = result['followers']
-        following = result['following']
-        caption = f"""**Info Of {name}**
-**Username:** `{username}`
-**Bio:** `{bio}`
-**Profile Link:** [Click Here]({url})
-**Company:** `{company}`
-**Created On:** `{created_at}`
-**Repositories:** `{repositories}`
-**Blog:** `{blog}`
-**Location:** `{location}`
-**Followers:** `{followers}`
-**Following:** `{following}`
-**@KoshikKumar17**"""
-    except Exception as e:
-        print(str(e))
-        pass
-    await message.reply_photo(photo=avatar_url, caption=caption, reply_markup=BUTTONS)
-    await bot.send_message(LOG_CHANNEL, A.format(update.from_user.mention, update.from_user.id))
