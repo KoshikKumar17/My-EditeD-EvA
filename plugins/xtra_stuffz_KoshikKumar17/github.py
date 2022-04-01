6d17cb8afce2ae35660f683fd954e0c1de76f511
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-01 11:16:16 +0530

    Update github.py

--- plugins/xtra_stuffz_KoshikKumar17/github.py
+++ plugins/xtra_stuffz_KoshikKumar17/github.py
@@ -19,19 +19,18 @@ async def getgithub(bot, message):
     URL = f'https://api.github.com/users/{un}'
     request = requests.get(URL)
     result = request.json()
-    try:
-        url = result['html_url']
-        name = result['name']
-        company = result['company']
-        bio = result['bio']
-        created_at = result['created_at']
-        avatar_url = result['avatar_url']
-        blog = result['blog']
-        location = result['location']
-        repositories = result['public_repos']
-        followers = result['followers']
-        following = result['following']
-        capy = f"""**Info Of {name}**
+    url = result['html_url']
+    name = result['name']
+    company = result['company']
+    bio = result['bio']
+    created_at = result['created_at']
+    avatar_url = result['avatar_url']
+    blog = result['blog']
+    location = result['location']
+    repositories = result['public_repos']
+    followers = result['followers']
+    following = result['following']
+    capy = f"""**Info Of {name}**
 **Username:** `{username}`
 **Bio:** `{bio}`
 **Profile Link:** [Click Here]({url})
@@ -44,9 +43,6 @@ async def getgithub(bot, message):
 **Following:** `{following}`
 
 **@KoshikKumar17**"""
-    except Exception as e:
-        print(str(e))
-        pass
     await message.reply_photo(photo=avatar_url, caption=capy, reply_markup=BUTTONS)
     await k.delete()
     await bot.send_message(LOG_CHANNEL, A.format(update.from_user.mention, update.from_user.id))
