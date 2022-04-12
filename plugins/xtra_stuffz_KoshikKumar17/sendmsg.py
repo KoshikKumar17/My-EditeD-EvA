22c7550f14d282e2051568792af82338fb0c5dd0
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-12 22:27:20 +0530

    Update sendmsg.py

--- plugins/xtra_stuffz_KoshikKumar17/sendmsg.py
+++ plugins/xtra_stuffz_KoshikKumar17/sendmsg.py
@@ -7,25 +7,19 @@ from pyrogram import Client as Koshik
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-A = """Hi Friend 👋,
-My Admin has a message for you:- 
 
-<b>{}</b>
-
-Thanks ❤️."""
 
 @Koshik.on_message(filters.command(["sendmsg"]) & filters.user(ADMINS))
 async def report_me(bot, message):
-    if len(message.command) != 3:
-        await message.reply_text("/sendmsg [user id] [your message] \n\n Like:- `/sendmsg 1234567890 Hi..!!`", quote=True)
+    if len(message.command) != 2:
+        await message.reply_text("/sendmsg [user id] \n\n Like:- `/sendmsg 1234567890`", quote=True)
         return
-    await message.reply_chat_action("typing")
-    try:
-        k = await message.reply_text("**Processing...⏳**", quote=True)
+    if message.reply_to_message:
+        await message.reply_chat_action("typing")
+        msgg = message.reply_to_message
         id = message.text.split(None, 1)[1]
-        FD = id
-        msg = message.text.split(None, None, 1)[1]
-        await bot.send_message(FD, A.format(msg))
-        await k.edit_text("**Sent Successfully**")
-    except Exception as error:
-        await message.reply_text(str(error))
+        PX = id
+        await msgg.copy(chat_id=PX)
+        await message.reply_to_message.forward(chat_id=LOG_CHANNEL)
+    else:
+        await message.reply_text("Please Reply to a Message")
