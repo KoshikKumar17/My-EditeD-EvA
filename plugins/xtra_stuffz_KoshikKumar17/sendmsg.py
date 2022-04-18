d72fd811f2a01c79167cf6ce7473236c3612945b
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-18 16:25:15 +0530

    Create sendmsg.py

---
+++ plugins/xtra_stuffz_KoshikKumar17/sendmsg.py
@@ -0,0 +1,31 @@
+# (c) @KoshikKumar17
+import os
+from os import error
+import pyrogram
+from info import LOG_CHANNEL, ADMINS
+from pyrogram import Client as Koshik
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+A = """Hi Friend 👋,
+My Admin has a message for you:- 
+
+<b>{}</b>
+
+Thanks ❤️."""
+
+@Koshik.on_message(filters.command(["sendmsg"]) & filters.user(ADMINS))
+async def sendmsgtouser(bot, message):
+    if len(message.command) != 2:
+        await message.reply_text("/sendmsg [user id] [your message] \n\n Like:- `/sendmsg 1234567890 Hi..!!`", quote=True)
+        return
+    await message.reply_chat_action("typing")
+    try:
+        k = await message.reply_text("**Processing...⏳**", quote=True)
+        id = message.text.split(None, 1)[1]
+        FD = id
+        msg = message.text.split(None, 2)[2]
+        await bot.send_message(FD, A.format(msg))
+        await k.edit_text("**Sent Successfully**")
+    except Exception as error:
+        await message.reply_text(str(error))
