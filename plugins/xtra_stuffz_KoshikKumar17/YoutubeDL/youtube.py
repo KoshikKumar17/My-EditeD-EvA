466cfb47e1e2bbb7d605314719975b43b7be96a2
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-21 11:04:35 +0530

    Create youtube.py

---
+++ plugins/xtra_stuffz_KoshikKumar17/YoutubeDL/youtube.py
@@ -0,0 +1,16 @@
+# By Koshik 🇮🇳
+# Thanks to TgExplore
+
+from datetime import datetime, timedelta
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from plugins.xtra_stuffz_KoshikKumar17.YoutubeDL.utils import extractYt, create_buttons
+import wget
+import os
+from PIL import Image
+
+
+
+
+
+More after
