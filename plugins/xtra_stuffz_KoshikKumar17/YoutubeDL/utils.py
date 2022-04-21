f944d140b8954f2c651288e7001b4a24e3a6f576
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2022-04-21 10:34:15 +0530

    Update utils.py

--- plugins/xtra_stuffz_KoshikKumar17/YoutubeDL/utils.py
+++ plugins/xtra_stuffz_KoshikKumar17/YoutubeDL/utils.py
@@ -1,7 +1,7 @@
 # By Koshik Kumar 🇮🇳
 # Thanks to TgExplore
 from __future__ import unicode_literals
-from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import Client, filters, StopPropagation, InlineKeyboardButton, InlineKeyboardMarkup
 import youtube_dl
 import asyncio
 import subprocess as sp
