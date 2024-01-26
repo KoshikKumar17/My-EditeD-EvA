afda118bab694fb62d4f8411e9d2856f76d32d66
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-01-26 08:38:41 +0530

    Update quotes.py

--- plugins/xtra_stuffz_KoshikKumar17/quotes.py
+++ plugins/xtra_stuffz_KoshikKumar17/quotes.py
@@ -13,35 +13,35 @@ BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('💖✨🇮🇳  Made By 
 
 @Client.on_message(filters.command("quote"))
 async def get_quote(bot, message):
-if len(message.command) != 2:
-    k = await message.reply_text("**Processing...⏳**")
-    await asyncio.sleep(2)
-    await k.delete()
-    URL = f'https://api.quotable.io/random'
-    request = requests.get(URL)
-    result = request.json()
-    qt = result['content']
-    athr = result['author']
-    tgs = result['tags']
-    gett_qt = f"""**“{qt}”**\n                        ~ {athr}\n\n**Category**:- {tgs}"""
-    await message.reply(
-        text=gett_qt,
-        disable_web_page_preview=True,
-        reply_markup = BUTTONS
-    )
-else:
-    k = await message.reply_text("**Processing...⏳**", quote=True)    
-    nu = message.text.split(None, 1)[1]
-    URL = f'https://api.quotable.io/random?tags={nu}'
-    request = requests.get(URL)
-    result = request.json()
-    qt = result['content']
-    athr = result['author']
-    tgs = result['tags']
-    gett_qt = f"""**“{qt}”**\n                      ~ {athr}\n\n**Category**:- {tgs}"""
-    await message.reply(
-        text=gett_qt,
-        disable_web_page_preview=True,
-        reply_markup = BUTTONS
-    )
-    await k.delete()
+    if len(message.command) != 2:
+        k = await message.reply_text("**Processing...⏳**", quote=True)
+        await asyncio.sleep(2)
+        await k.delete()
+        URL = f'https://api.quotable.io/random'
+        request = requests.get(URL)
+        result = request.json()
+        qt = result['content']
+        athr = result['author']
+        tgs = result['tags']
+        gett_qt = f"""**“{qt}”**\n                        ~ {athr}\n\n**Category**:- {tgs}"""
+        await message.reply(
+            text=gett_qt,
+            disable_web_page_preview=True,
+            reply_markup = BUTTONS
+        )
+    else:
+        k = await message.reply_text("**Processing...⏳**", quote=True)
+        nu = message.text.split(None, 1)[1]
+        URL = f'https://api.quotable.io/random?tags={nu}'
+        request = requests.get(URL)
+        result = request.json()
+        qt = result['content']
+        athr = result['author']
+        tgs = result['tags']
+        gett_qt = f"""**“{qt}”**\n                      ~ {athr}\n\n**Category**:- {tgs}"""
+        await message.reply(
+            text=gett_qt,
+            disable_web_page_preview=True,
+            reply_markup = BUTTONS
+        )
+        await k.delete()
