5e4c1f01be9462466748b108459db4b3e42f15ec
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-02-04 09:32:54 +0530

    Update spotify.py

--- plugins/xtra_stuffz_KoshikKumar17/spotify.py
+++ plugins/xtra_stuffz_KoshikKumar17/spotify.py
@@ -9,15 +9,18 @@ from pyrogram import filters
 
 @Client.on_message(filters.command("spotify"))
 async def spotifydl(bot, message):
+    k = await message.reply_text("**Processing...⏳**", quote=True)
     if not message.reply_to_message:
-        return await message.reply_text("**Reply to some Spotify link.**", quote=True)
+        return await k.edit_text("**Reply to some Spotify link.**", quote=True)
     if not message.reply_to_message.text:
-        return await message.reply_text("**Reply to some Spotify link.**", quote=True)
+        return await k.edit_text("**Reply to some Spotify link.**", quote=True)
     if ("album" in message.reply_to_message.text) or ("playlist" in message.reply_to_message.text):
-        return await message.reply_text("**Album or Playlist links not supported.**", quote=True)
+        return await k.edit_text("**Album or Playlist links not supported.**", quote=True)
     url = requests.get(f"{message.reply_to_message.text}", allow_redirects=True).url
-    api1 = f"https://api.fabdl.com/spotify/get?url={url}"
-    response = 
+    API1 = f"https://api.fabdl.com/spotify/get?url={url}"
+    request = requests.get(API1)
+    result1 = request.json()
+    
     
 
 
