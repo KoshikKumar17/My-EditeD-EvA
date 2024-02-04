a2218290eff89155c486c4533d5d4d4f5cf11ff9
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-02-04 09:28:32 +0530

    Update spotify.py

--- plugins/xtra_stuffz_KoshikKumar17/spotify.py
+++ plugins/xtra_stuffz_KoshikKumar17/spotify.py
@@ -15,6 +15,9 @@ async def spotifydl(bot, message):
         return await message.reply_text("**Reply to some Spotify link.**", quote=True)
     if ("album" in message.reply_to_message.text) or ("playlist" in message.reply_to_message.text):
         return await message.reply_text("**Album or Playlist links not supported.**", quote=True)
+    url = requests.get(f"{message.reply_to_message.text}", allow_redirects=True).url
+    api1 = f"https://api.fabdl.com/spotify/get?url={url}"
+    response = 
     
 
 
