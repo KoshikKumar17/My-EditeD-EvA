c67b59b195adea1945bc2d5a503c0fa27b9ad4f2
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-02-04 08:49:26 +0530

    Update spotify.py

--- plugins/xtra_stuffz_KoshikKumar17/spotify.py
+++ plugins/xtra_stuffz_KoshikKumar17/spotify.py
@@ -7,3 +7,13 @@ import json
 from pyrogram import Client
 from pyrogram import filters
 
+@Client.on_message(filters.command("spotify"))
+async def spotifydl(bot, message):
+    if not message.reply_to_message:
+        return await message.reply_text("**Reply to some Spotify link.**", quote=True)
+    if not message.reply_to_message.text:
+        return await message.reply_text("**Reply to some Spotify link.**", quote=True)
+
+
+
+  
