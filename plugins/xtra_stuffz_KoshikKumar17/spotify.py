795c653d1138a0c4099e4aa0ef0f713e2d1f78dc
Koshik Kumar <88644185+KoshikKumar17@users.noreply.github.com>
2024-02-04 10:36:05 +0530

    Update spotify.py

--- plugins/xtra_stuffz_KoshikKumar17/spotify.py
+++ plugins/xtra_stuffz_KoshikKumar17/spotify.py
@@ -11,11 +11,11 @@ from pyrogram import filters
 async def spotifydl(bot, message):
     k = await message.reply_text("**Processing...⏳**", quote=True)
     if not message.reply_to_message:
-        return await k.edit_text("**Reply to some Spotify link.**", quote=True)
+        return await k.edit_text("**Reply to some Spotify link.**")
     if not message.reply_to_message.text:
-        return await k.edit_text("**Reply to some Spotify link.**", quote=True)
+        return await k.edit_text("**Reply to some Spotify link.**")
     if ("album" in message.reply_to_message.text) or ("playlist" in message.reply_to_message.text):
-        return await k.edit_text("**Album or Playlist links not supported.**", quote=True)
+        return await k.edit_text("**Album or Playlist links not supported.**")
     url = requests.get(f"{message.reply_to_message.text}", allow_redirects=True).url
     API1 = f"https://api.fabdl.com/spotify/get?url={url}"
     request = requests.get(API1)
